532b6c4a4797225375b6481d22ebff3624f753a4
FluffyQuack <git@fluffyquack.com>
2021-12-06 16:39:21 +0100

    Updated one of the build batch files

--- src/fluffy-build-step2.bat
+++ src/fluffy-build-step2.bat
@@ -1,3 +1,4 @@
 set "SDL2=H:\apps\code\libcurl\vcpkg\packages\sdl2_x86-windows"
 set "SDL2_Image=H:\apps\code\libcurl\vcpkg\packages\sdl2-image_x86-windows"
-build release
+REM build release
+build debug
