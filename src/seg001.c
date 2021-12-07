dfc860e16779ef44c8a0854fbf28d512752d212f
FluffyQuack <git@fluffyquack.com>
2021-12-07 14:36:04 +0100

    (MultiRoomRendering)
    Fixed rendering of cutscenes.

--- src/seg001.c
+++ src/seg001.c
@@ -656,6 +656,20 @@ void __pascal far expired() {
 
 // seg001:0CCD
 void __pascal far load_intro(int which_imgs,cutscene_ptr_type func,int free_sounds) {
+
+	//Fluffy (MultiRoomRendering): Turn off everything related to multi-room rendering for cutscenes
+	renderPosOffsetPrevious = renderPosOffsetTarget = 0.0f;
+	SDL_FillRect(onscreen_surface_right, NULL, 0);
+	SDL_UpdateTexture(texture_sharp_right_ptr, NULL, onscreen_surface_right->pixels, onscreen_surface_right->pitch);
+	SDL_UpdateTexture(texture_sharp_left_ptr, NULL, onscreen_surface_right->pixels, onscreen_surface_right->pitch);
+	texture_sharp_right_needUpload = 0;
+	texture_sharp_left_needUpload = 0;
+
+	//Fluffy (MultiRoomRendering): Turn off all trobs
+	for (int index = 0; index < trobs_count; ++index)
+		trobs[index].type = -1;
+	trobs_count = 0;
+
 	short current_star;
 	draw_rect(&screen_rect, 0);
 	if (free_sounds) {
